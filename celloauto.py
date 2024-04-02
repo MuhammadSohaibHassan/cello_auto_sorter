81203397909ea4e99880892ace868e3eaee68d62
MuhammadSohaibHassan <124514984+MuhammadSohaibHassan@users.noreply.github.com>
2024-04-02 18:21:21 +0500

    Update celloauto.py

--- celloauto.py
+++ celloauto.py
@@ -7,6 +7,10 @@ cus = """
     visibility:hidden;
 }
 
+#root > div:nth-child(1) > div > div > button{
+    visibility:hidden;
+}
+
 #root > div:nth-child(1) > div.withScreencast > div > div > div > section{
     background:purple;
     }
