f75416a8f610068b35ced03b98b7f7cb76eaab37
MuhammadSohaibHassan <124514984+MuhammadSohaibHassan@users.noreply.github.com>
2024-04-05 08:48:33 +0500

    Update celloauto.py

--- celloauto.py
+++ celloauto.py
@@ -8,7 +8,7 @@ cus = """
     }
     
 #root > div:nth-child(1) > div.withScreencast > div > div > div > section.st-emotion-cache-1ec6rqw.eczjsme11 > div.st-emotion-cache-6qob1r.eczjsme3{
-    background:lightgreen;
+    background:darkseagreen;
 }
 
 </style>
